a7229bead887f203f01e28355163c84de883451f
adcoolguy <adcoolguy>
2016-09-10 19:37:52 +0800

    Fix cache isue of github

--- pp.cmd
+++ pp.cmd
@@ -1,1 +1,4 @@
-git push
+echo If needed, uncomment and launch this in an Administrator DOS prompt to unset!
+:git config --system --unset credential.helper
+:git config --unset-all user.name
+git config credential.helper store && git config user.name adcoolguy&& git config user.email adcoolguy&& git push
