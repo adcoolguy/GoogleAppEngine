935cb68ec4c84f5e6cb1325c2c24991fc8580b75
adcoolguy <adcoolguy>
2017-11-10 22:18:57 -0500

    build for the latest sdk on Windows

--- src/com/appspot/cloudserviceapi/springmvc/controller/CustomXmlWebApplicationContext.java
+++ src/com/appspot/cloudserviceapi/springmvc/controller/CustomXmlWebApplicationContext.java
@@ -3,8 +3,8 @@ package com.appspot.cloudserviceapi.springmvc.controller;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
-import com.google.appengine.api.utils.SystemProperty;
-
+// import com.google.appengine.api.utils.SystemProperty;
+//=== to avoidd Error: java.lang.NoClassDefFoundError: com/google/appengine/api/utils/SystemProperty
 /*
  * https://cloud.google.com/appengine/articles/spring_optimization
  */
@@ -12,9 +12,9 @@ public class CustomXmlWebApplicationContext extends XmlWebApplicationContext {
 	protected void initBeanDefinitionReader(
 			XmlBeanDefinitionReader beanDefinitionReader) {
 		super.initBeanDefinitionReader(beanDefinitionReader);
-		if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+		// if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
 			beanDefinitionReader.setValidating(false);
 			beanDefinitionReader.setNamespaceAware(true);
-		}
+		// }
 	}
 }
