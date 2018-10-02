8a6976ca12cc6c60ef98c418e7f2faee329ae07b
adcoolguy <adcoolguy>
2018-10-03 07:58:02 +0800

    Trying to set up command line data enhancer

--- bin/e.cmd
+++ bin/e.cmd
@@ -3,6 +3,8 @@ call setenv.bat
 set GAE_JAVA_SDK_HOME=C:\appengine-java-sdk-1.9.65
 set TCLASSPATH=..\war\WEB-INF\classes_
 
+REM https://cloud.google.com/appengine/docs/standard/java/datastore/jdo/overview
+
 java -cp %GAE_JAVA_SDK_HOME%\lib\appengine-tools-api.jar;%TCLASSPATH%;%PROJECT_HOME%\mygae.jar com.google.appengine.tools.enhancer.Enhance %TCLASSPATH%\tapp\model\*.class 
 
 rem %TCLASSPATH%\com\appspot\cloudserviceapi\common\*.class
