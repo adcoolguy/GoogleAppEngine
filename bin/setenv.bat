016b449ab177a9b9890d81549c1fce40bcfe69aa
adcoolguy <androidcoolguy@hotmail.com>
2018-10-02 08:51:08 +0800

    Started work to make apps run on Java 8 and the latest GAEJ runtime

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -4,7 +4,7 @@ set PROJECT_HOME=..
 
 set JAVA_HOME=\jdk1.7.0_80
 rem "Please make sure your JDK is set correctly as above, i.e. in the root directory! Otherwise, JSP compilation will fail."
-set GAE_JAVA_SDK_HOME=\appengine-java-sdk-1.9.59
+set GAE_JAVA_SDK_HOME=\appengine-java-sdk-1.9.65
 set M2_HOME=\apache-maven-3.3.9
 set ROO_HOME=\spring-roo-1.1.0.M3
 set ANT_HOME=\apache-ant-1.6.5
