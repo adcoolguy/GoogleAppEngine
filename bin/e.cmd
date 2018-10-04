5fac8be7a4eb6e1660a7771a33e05e6848fc229f
adcoolguy <adcoolguy>
2018-10-04 08:37:40 +0800

    Fixed run time error with Java 7 / Jetty 6

--- bin/e.cmd
+++ bin/e.cmd
@@ -8,6 +8,6 @@ REM https://cloud.google.com/appengine/docs/standard/java/datastore/jdo/overview
 
 REM WARNING: Requires Java 8 runtime!!!
 
-java -cp %GAE_JAVA_SDK_HOME%\lib\appengine-tools-api.jar;%GAE_JAVA_SDK_HOME%\lib\user\appengine-api-1.0-sdk-1.9.65.jar;%TCLASSPATH%;%PROJECT_HOME%\mygae.jar com.google.appengine.tools.enhancer.Enhance %TCLASSPATH%\app\model\*.class %TCLASSPATH%\tapp\model\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\common\model\*.class
+%JAVA_HOME%\bin\java -cp %TCLASSPATH%;%GAE_JAVA_SDK_HOME%\lib\appengine-tools-api.jar;%GAE_JAVA_SDK_HOME%\lib\user\appengine-api-1.0-sdk-1.9.65.jar;%TCLASSPATH%;%PROJECT_HOME%\mygae.jar com.google.appengine.tools.enhancer.Enhance %TCLASSPATH%\app\model\*.class %TCLASSPATH%\tapp\model\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\common\model\*.class %TCLASSPATH%\com\appspot\cloudserviceapi\security\spring\model\GaeDaoAuthenticationProvider.class
 
 rem 
